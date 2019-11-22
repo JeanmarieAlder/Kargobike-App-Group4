c91b7419c3baf5fc8d23b6796c5f60eed21c9473
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2019-11-22 13:04:44 +0100

    File structure 2

--- app/src/main/java/com/example/kargobikeappg4/ui/nav/WelcomeActivity.java
+++ app/src/main/java/com/example/kargobikeappg4/ui/nav/WelcomeActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.kargobikeappg4.R;
+import com.example.kargobikeappg4.ui.product.ProductListActivity;
 import com.example.kargobikeappg4.ui.transport.TransportListActivity;
+import com.example.kargobikeappg4.ui.user.ProfileActivity;
 import com.example.kargobikeappg4.ui.user.UserListActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -22,6 +24,18 @@ public class WelcomeActivity extends AppCompatActivity {
     public void transportButton(View view)
     {
         Intent intent = new Intent(this, TransportListActivity.class);
+        startActivity(intent);
+    }
+
+    public void productButton(View view)
+    {
+        Intent intent = new Intent(this, ProductListActivity.class);
+        startActivity(intent);
+    }
+
+    public void profileButton(View view)
+    {
+        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
 
