966eabaaaffa0979c987ef2c3953bf2b705bc4bb
AgronAsani <agron.asani@students.hevs.ch>
2019-12-05 12:50:20 +0100

    Login with google

--- app/src/main/java/com/example/kargobikeappg4/ui/login/Register.java
+++ app/src/main/java/com/example/kargobikeappg4/ui/login/Register.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.kargobikeappg4.R;
+import com.example.kargobikeappg4.ui.nav.BikeConfirmationActivity;
 import com.example.kargobikeappg4.ui.nav.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,66 +23,59 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
-    EditText account, password;
-    Button  btnRegister;
-    FirebaseAuth fAuth;
-    ProgressBar progressBar;
+    EditText emailId, password;
+    Button btnSignUp;
+    TextView tvSignIn;
+    FirebaseAuth mFirebaseAuth;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        account = findViewById(R.id.edit_RegName);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.edit_RegName);
         password = findViewById(R.id.edit_RegPwd);
-        btnRegister = findViewById(R.id.btn_RegNew);
+        btnSignUp = findViewById(R.id.btn_RegNew);
 
-        fAuth = FirebaseAuth.getInstance();
-        progressBar = findViewById(R.id.progressBar);
-
-        if(fAuth.getCurrentUser() != null){
-            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-            finish();
-        }
-
-        btnRegister.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                String acc = account.getText().toString().trim();
-                String pwd = password.getText().toString().trim();
-
-                if(TextUtils.isEmpty(acc)){
-                    account.setError("Email is Required");
-                    return;
-                }
-
-                if(TextUtils.isEmpty(pwd)){
-                    password.setError("Password is Required");
-                    return;
-                }
-
-                progressBar.setVisibility(View.VISIBLE);
-
-                fAuth.createUserWithEmailAndPassword(acc,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                        }else{
-                            Toast.makeText(Register.this, "Error!", Toast.LENGTH_SHORT).show();
-                        }
+        btnSignUp.setOnClickListener(v -> {
+            String email = emailId.getText().toString();
+            String pwd = password.getText().toString();
+            if(email.isEmpty()){
+                emailId.setError("Please enter email id");
+                emailId.requestFocus();
+            }
+            else  if(pwd.isEmpty()){
+                password.setError("Please enter your password");
+                password.requestFocus();
+            }
+            else  if(email.isEmpty() && pwd.isEmpty()){
+                Toast.makeText(Register.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+            }
+            else  if(!(email.isEmpty() && pwd.isEmpty())){
+                mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(Register.this, task -> {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(Register.this,"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        startActivity(new Intent(Register.this, BikeConfirmationActivity.class));
                     }
                 });
+            }
+            else{
+                Toast.makeText(Register.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
 
             }
-
+        });
+        /*
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Register.this,LoginActivity.class);
+                startActivity(i);
+            }
         });
 
-
-
+         */
     }
-
-
-
 }
--- app/src/main/java/com/example/kargobikeappg4/ui/nav/LoginActivity.java
+++ app/src/main/java/com/example/kargobikeappg4/ui/nav/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.kargobikeappg4.ui.nav;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -18,8 +20,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -27,18 +31,30 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class LoginActivity extends AppCompatActivity {
 
     static final int GOOGLE_SIGN = 123;
-    Button login;
+    Button loginGoogle;
     FirebaseAuth mAuth;
     GoogleSignInClient google;
     ProgressBar progressBar;
+
+    EditText email;
+
+    Button loginAccount;
+    EditText emailId, password;
+
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login = findViewById(R.id.btn_login);
+        loginGoogle = findViewById(R.id.btn_login);
+        loginAccount = findViewById(R.id.btn_signIn);
         progressBar = findViewById(R.id.progressBar);
+        email = findViewById(R.id.edit_Naccount);
+        password = findViewById(R.id.edit_Npassword);
         mAuth = FirebaseAuth.getInstance();
+
 
         GoogleSignInOptions googleOptions = new GoogleSignInOptions.Builder()
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -47,12 +63,70 @@ public class LoginActivity extends AppCompatActivity {
 
         google = GoogleSignIn.getClient(this, googleOptions);
 
-        login.setOnClickListener(v -> signInGoogle());
+        loginGoogle.setOnClickListener(v -> signInGoogle());
+
+        loginAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty()){
+                    emailId.setError("Please enter email id");
+                    emailId.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    password.setError("Please enter your password");
+                    password.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(LoginActivity.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Intent intToHome = new Intent(LoginActivity.this,BikeConfirmationActivity.class);
+                                startActivity(intToHome);
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
 
         if(mAuth.getCurrentUser() != null){
             FirebaseUser user = mAuth.getCurrentUser();
             updateUI(user);
         }
+
+
+/*
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(LoginActivity.this,"You are logged in",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, BikeConfirmationActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Please Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+*/
 
     }
 
@@ -101,7 +175,8 @@ public class LoginActivity extends AppCompatActivity {
                         FirebaseUser user = mAuth.getCurrentUser();
                         updateUI(user);
 
-                        riderSelected();
+                        Intent intent = new Intent(this, BikeConfirmationActivity.class);
+                        startActivity(intent);
                     }else{
                         progressBar.setVisibility(View.INVISIBLE);
                         Log.d("TAG", "signin failure", task.getException());
@@ -122,7 +197,7 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void riderSelected()
+    public void riderSelected(View view)
     {
         Intent intent = new Intent(this, BikeConfirmationActivity.class);
         startActivity(intent);
@@ -132,5 +207,10 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
     }
-
+    /*
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+*/
 }
