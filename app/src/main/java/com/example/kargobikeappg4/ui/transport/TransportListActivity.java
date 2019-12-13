d004ea687236390eecb9846f167fa5730b890fca
damianwasmer <damian.wasmer@students.hevs.ch>
2019-12-13 14:34:53 +0100

    Filter the Orderlist (by the current user)

--- app/src/main/java/com/example/kargobikeappg4/ui/transport/TransportListActivity.java
+++ app/src/main/java/com/example/kargobikeappg4/ui/transport/TransportListActivity.java
@@ -17,6 +17,7 @@ import com.example.kargobikeappg4.db.entities.Order;
 import com.example.kargobikeappg4.db.entities.User;
 import com.example.kargobikeappg4.util.RecyclerViewItemClickListener;
 import com.example.kargobikeappg4.viewmodel.order.OrderListViewModel;
+import com.example.kargobikeappg4.viewmodel.user.UserViewModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -32,9 +33,11 @@ public class TransportListActivity extends AppCompatActivity {
     private List<Order> orders;
     private List<Order> filteredorders;
     private OrderListViewModel listViewModel;
+    private UserViewModel userViewmodel;
     private RecyclerView rView;
     private FirebaseAuth fAuth;
     private FirebaseUser fUser;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +48,17 @@ public class TransportListActivity extends AppCompatActivity {
         fAuth = FirebaseAuth.getInstance();
         fUser = fAuth.getCurrentUser();
         Log.d("User Firebase", "User id (current)" + fUser.getUid() + fUser.getDisplayName() );
+
+        UserViewModel.Factory factory = new UserViewModel.Factory(
+                getApplication(), fUser.getUid());
+        userViewmodel = ViewModelProviders.of(this, factory)
+                .get(UserViewModel.class);
+        userViewmodel.getUser().observe(this, userEntitie ->{
+            if(userEntitie != null)
+            {
+                user = userEntitie;
+            }
+        });
 
         //initializes recyclerview
         rView = findViewById(R.id.recycler_view_storage);
@@ -66,16 +80,22 @@ public class TransportListActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        OrderListViewModel.Factory factory = new OrderListViewModel.Factory(
+        OrderListViewModel.Factory factory2 = new OrderListViewModel.Factory(
                 getApplication()
         );
 
-        listViewModel = ViewModelProviders.of(this, factory)
+        listViewModel = ViewModelProviders.of(this, factory2)
                 .get(OrderListViewModel.class);
         listViewModel.getAllOrders().observe(this, orderEntities -> {
             if (orderEntities != null) {
                 orders = orderEntities;
-                adapter.setData(orders);
+                filteredorders = new ArrayList<>();
+                for (Order o : orders) {
+                    if (o.getIdResponsibleRider().equals(user.getName())) {
+                        filteredorders.add(o);
+                    }
+                }
+                adapter.setData(filteredorders);
             }
         });
         rView.setAdapter(adapter);
