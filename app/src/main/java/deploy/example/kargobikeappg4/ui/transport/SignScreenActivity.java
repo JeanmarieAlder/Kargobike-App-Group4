f62191b2065215bd94676b8ef5d6e68b02a4c5fc
AgronAsani <agron.asani@students.hevs.ch>
2019-12-18 18:41:18 +0100

    Signature Screen- not complete

--- app/src/main/java/deploy/example/kargobikeappg4/ui/transport/SignScreenActivity.java
+++ app/src/main/java/deploy/example/kargobikeappg4/ui/transport/SignScreenActivity.java
@@ -1,17 +1,78 @@
-
 package deploy.example.kargobikeappg4.ui.transport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 import deploy.example.kargobikeappg4.R;
+import deploy.example.kargobikeappg4.ui.transport.paint.PaintView;
 
-public class SignScreenActivity extends AppCompatActivity {
+public class SignScreenActivity extends AppCompatActivity{
+    private PaintView paintView;
+    public Bitmap signature;
+    private Button btnSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_screen);
+
+
+        paintView = (PaintView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+
+        btnSave = findViewById(R.id.btn_signature_ok);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paintView.save();
+            }
+        });
     }
+
+
+
+
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.normal:
+                paintView.normal();
+                return true;
+            case R.id.emboss:
+                paintView.emboss();
+                return true;
+            case R.id.blur:
+                paintView.blur();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+ */
 }
