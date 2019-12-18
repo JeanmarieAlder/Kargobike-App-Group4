f6b79599ec756f923e4da23955fada8d694664cf
Mouzet <mermod.yannick@gmail.com>
2019-12-18 13:22:29 +0100

    Button about in profile page display only if the user see his own profile

---
+++ app/src/main/java/deploy/example/kargobikeappg4/ui/user/AboutActivity.java
@@ -0,0 +1,17 @@
+package deploy.example.kargobikeappg4.ui.user;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import deploy.example.kargobikeappg4.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
