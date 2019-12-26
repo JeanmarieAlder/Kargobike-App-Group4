5c4476ddf17b90cd642d8d296a09776bf4a9b464
AgronAsani <agron.asani@students.hevs.ch>
2019-12-26 14:23:11 +0100

    Photo Screen - capture images works

--- app/src/main/java/deploy/example/kargobikeappg4/ui/transport/PhotoScreenActivity.java
+++ app/src/main/java/deploy/example/kargobikeappg4/ui/transport/PhotoScreenActivity.java
@@ -1,27 +1,63 @@
 package deploy.example.kargobikeappg4.ui.transport;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.audiofx.AudioEffect;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.security.Permission;
 
 import deploy.example.kargobikeappg4.R;
 
 public class PhotoScreenActivity extends AppCompatActivity {
     Button camera;
+    ImageView imageView;
+
+    Uri image_uri;
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_screen);
 
-        camera = (Button)findViewById(R.id.btn_camera);
+        imageView = findViewById(R.id.img_picture);
+        camera = findViewById(R.id.btn_camera);
+
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED){
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }else{
+                        openCamera();
+                    }
+
+                }else{
+                    openCamera();
+
+                }
+                /*
                 try{
                     Intent intent = new Intent();
                     intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -31,7 +67,41 @@ public class PhotoScreenActivity extends AppCompatActivity {
                     e.printStackTrace();
 
                 }
+
+                 */
             }
         });
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the cammera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int [] grantResults){
+        switch(requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+
+                }else{
+                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            imageView.setImageURI(image_uri);
+        }
     }
 }
